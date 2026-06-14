898f9ebcd9060cbadc2e71abc5aecf5a2600662e
DavidGitCoder <github@solutionzmail.com>
2026-06-14 15:02:53 +0200

    add a main application window

--- Tutorial01/app.py
+++ Tutorial01/app.py
@@ -1,14 +1,24 @@
-from PySide6.QtWidgets import QApplication,QWidget
-
 #only needed to access command line arguments
 import sys
+
+from PySide6.QtCore import QSize, Qt
+from PySide6.QtWidgets import QApplication,QMainWindow, QPushButton
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My First App")
+        button=QPushButton("Press Me!")
+        self.setCentralWidget(button)
+
+
 # You need one (and only one) QApplication instance per application.
 # Pass in sys.argv to allow command line arguments for you app.
 # If you know yo won't use command line arguments QApplication([]) works too.
 app=QApplication(sys.argv)
 
 # Create a Qt widget, which will be our window
-window=QWidget()
+window=QMainWindow()
 window.show() # IMPORTANT!!!! Windows are hidden by default
 
 # Start the event loop
